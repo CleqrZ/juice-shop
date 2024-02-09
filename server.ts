b8992fbed9620b106a26ee0d17c23329d762086f
CleqrZ <dylangrangeon34@gmail.com>
2024-02-09 15:39:47 +0100

    Update server.ts
    Mise en place du blocage de modification des produits pour tout les utilisateurs

--- server.ts
+++ server.ts
@@ -336,7 +336,10 @@ restoreOverwrittenFilesWithOriginals().then(() => {
     .delete(security.denyAll())
   /* Products: Only GET is allowed in order to view products */ // vuln-code-snippet neutral-line changeProductChallenge
   app.post('/api/Products', security.isAuthorized()) // vuln-code-snippet neutral-line changeProductChallenge
-  // app.put('/api/Products/:id', security.isAuthorized()) // vuln-code-snippet vuln-line changeProductChallenge
+  // Bloquer l'accès à la modification de produits pour tous les utilisateurs
+  app.put('/api/Products/:id', (req: Request, res: Response, next: NextFunction) => {
+    res.status(403).json({ message: "Forbidden" });
+  });
   app.delete('/api/Products/:id', security.denyAll())
   /* Challenges: GET list of challenges allowed. Everything else forbidden entirely */
   app.post('/api/Challenges', security.denyAll())
